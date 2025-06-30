f8fba88b3afdff294bb9f8e6575ec0ede9b6ffe0
Sarab23y <sba431@uky.edu>
2025-06-30 14:13:14 -0400

    debug2

--- python_codes/LV_simulation/dependencies/nsolver.py
+++ python_codes/LV_simulation/dependencies/nsolver.py
@@ -135,10 +135,9 @@ class NSolver(object):
                     w.vector().axpy(1.0, dww.vector())
 
                     #SARA: check for NaNs
-                    w_vec = w.vector().get_local()
-                    if np.isnan(w_vec).any():
-                        print("NaN in solution vector 'w' at iteration %d on rank %d" % (it, self.comm.Get_rank()))
-                        np.save("crash_w_rank%d_it%d.npy" % (self.comm.Get_rank(), it), w_vec)
+                    if np.isnan(w.vector().get_local()).any():
+                        print("NaN found in w at iteration", it, "on rank", self.comm.Get_rank())
+
 
 
 
@@ -162,10 +161,9 @@ class NSolver(object):
                     #    print "nan found in B assembly after bcs"
 
                         #SARA: check for Nans in B
-                    local_B = B.get_local()
-                    if np.isnan(local_B).any():
-                        print("NaN detected in residual vector B at iteration %d on rank %d" % (it, self.comm.Get_rank()))
-                        print("Residual norm: %.3e, rel_res: %.3e" % (B.norm("l2"), rel_res))
+                    if np.isnan(B.get_local()).any():
+                        print("NaN found in residual vector B at iteration", it, "on rank", self.comm.Get_rank())
+
 
 
                     rel_res = B.norm("l2")/resid0
@@ -297,18 +295,11 @@ class NSolver(object):
                     self.comm.Barrier()
 
                     #SARA: Dump state before raising RuntimeError
+            
                 if((rel_res > rel_tol and res > abs_tol) or  math.isnan(res)):
-                    if self.comm.Get_rank() == 0:
-                        print("Convergence failed at iteration %d" % it)
-                        print("Final residual: %.3e, relative: %.3e" % (res, rel_res))
-                        print("Dumping state...")
-
-                    np.save("debug_w_rank%d.npy" % self.comm.Get_rank(), w.vector().get_local())
-                    raise RuntimeError("Failed Convergence")
-
-               """ if((rel_res > rel_tol and res > abs_tol) or  math.isnan(res)):
+                    print("Convergence failed at iteration", it, "on rank", self.comm.Get_rank())
                     #self.parameters["FileHandler"][4].close()
-                    raise RuntimeError("Failed Convergence")"""
+                    raise RuntimeError("Failed Convergence")
     def solvenonlinear(self):
 
         abs_tol = self.solver_params["abs_tol"]
@@ -463,10 +454,10 @@ class NSolver(object):
                     #solve(A, dww.vector(), b,solver_parameters={"linear_solver": "gmres",
                     #        "preconditioner": "hypre_euclid"})
                     w.vector().axpy(1.0, dww.vector())
-                    w_vec = w.vector().get_local()
-                    if np.isnan(w_vec).any():
-                        print("NaN in solution vector 'w' at iteration %d on rank %d" % (it, self.comm.Get_rank()))
-                        np.save("crash_w_rank%d_it%d.npy" % (self.comm.Get_rank(), it), w_vec)
+
+                    #SARA
+                    if np.isnan(w.vector().get_local()).any():
+                        print("NaN found in w at iteration", it, "on rank", self.comm.Get_rank())
 
 
 
@@ -485,10 +476,8 @@ class NSolver(object):
 
                     for bc in bcs:
                             bc.apply(B)
-                    local_B = B.get_local()
-                    if np.isnan(local_B).any():
-                        print("NaN detected in residual vector B at iteration %d on rank %d" % (it, self.comm.Get_rank()))
-                        print("Residual norm: %.3e, rel_res: %.3e" % (B.norm("l2"), rel_res))
+                    if np.isnan(B.get_local()).any():
+                        print("NaN found in residual vector B at iteration", it, "on rank", self.comm.Get_rank())
 
                     #if np.isnan(B.array().astype(float)).any():
                     #    print "nan found in B assembly after bcs"
@@ -615,12 +604,7 @@ class NSolver(object):
                         if np.isnan(b.array().astype(float)).any():
                             print 'nan found in b (Ftotal) assembly\n'
                     self.comm.Barrier()
-                """if((rel_res > rel_tol and res > abs_tol) or  math.isnan(res)):
+                if((rel_res > rel_tol and res > abs_tol) or  math.isnan(res)):
                     #self.parameters["FileHandler"][4].close()
-                    raise RuntimeError("Failed Convergence")"""
-                if((rel_res > rel_tol and res > abs_tol) or math.isnan(res)):
-                    if self.comm.Get_rank() == 0:
-                        print("Convergence failed at iteration %d" % it)
-                        print("Final residual: %.3e, relative: %.3e" % (res, rel_res))
-                    np.save("debug_w_rank%d.npy" % self.comm.Get_rank(), w.vector().get_local())
                     raise RuntimeError("Failed Convergence")
+                
